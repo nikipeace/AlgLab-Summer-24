6a482bef12b80eeecb7bd71849ee50cce9918c91
Nikolai <n.schoetz@tu-bs.de>
2024-04-03 15:35:46 +0200

    00_cpsat

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -7,9 +7,24 @@ def solve(instance: Instance) -> Solution:
     Implement your solver for the problem here!
     """
     numbers = instance.numbers
+    min_num = min(numbers)
+    max_num = max(numbers)
+
     model = cp_model.CpModel()
+
+    x = model.NewIntVar(min_num, max_num, "x")
+    y = model.NewIntVar(min_num, max_num, "y")
+
+    model.Maximize(x-y)
+
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+
+    assert (status == cp_model.OPTIMAL)
+    print(f"x={solver.Value(x)},  y={solver.Value(y)}")
+
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=solver.Value(y),
+        number_b=solver.Value(x),
+        distance=abs(solver.Value(y) - solver.Value(x)),
     )
